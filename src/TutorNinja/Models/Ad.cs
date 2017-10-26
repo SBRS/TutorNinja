8178f006c93602f99a2a61636cad45dea682bc5d
#SpaceBoundRocketship <alexli0612@gmail.com>
2017-10-26 13:05:39 +1300

    Ad model

--- src/TutorNinja/Models/Ad.cs
+++ src/TutorNinja/Models/Ad.cs
@@ -10,12 +10,12 @@ namespace TutorNinja.Models
     {
         public int AdID { get; set; }        
         public int CategoryID { get; set; }
-        [Column(TypeName = "money")]
         public decimal Price { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public DateTime CreateDate { get; set; }
 
         public Category Category { get; set; }
+        public ApplicationUser User { get; set; }
     }
 }
